5e19270e59fb20d015b406a88d020c0577edd0e4
LauraRhodes <laura@pyxiseng.com>
2019-10-17 16:26:38 -0700

    First commit of Minibot code

---
+++ Minibot/Nika_First_Project/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private Joystick m_Stick;
+  private AnalogInput m_potentiometer;
+
+  @Override
+  public void robotInit() {
+    m_myRobot = new DifferentialDrive(new WPI_TalonSRX(0), new WPI_TalonSRX(15));
+    m_Stick = new Joystick(0);
+    m_potentiometer=new AnalogInput(1);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  
+    m_myRobot.arcadeDrive(m_Stick.getY(), m_Stick.getTwist());
+  //  m_myRobot.tankDrive(m_Stick.getY(), m_Stick.getThrottle());
+  // System.out.println(m_potentiometer.getVoltage());
+  SmartDashboard.putNumber("pot", m_potentiometer.getVoltage());
+  SmartDashboard.putNumber("left_Y", m_Stick.getY());
+  SmartDashboard.putNumber("right_X", m_Stick.getThrottle());
+}
+}
