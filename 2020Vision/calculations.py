70fda7543f668f90666ae001723684f9d6379818
Alex Krantz <alex@alexkrantz.com>
2020-01-06 17:50:37 -0800

    Reduce constants in favor of calculation during runtime

--- 2020Vision/calculations.py
+++ 2020Vision/calculations.py
@@ -5,10 +5,11 @@ TARGET_HEIGHT = 17
 
 # Get the angle to a point rounded to 4 decimal places
 def angle(config, x):
-    return round((x - config.image_center.x) * config.horizontal_degrees_per_pixel, 4)
+    return round((x - (config.camera.width/2) - 0.5) * (config.camera.fov.horizontal / config.camera.width), 4)
 
 
 # Get the distance to the target
+# This is going to be replaced by the Intel RealSense
 def distance(config, y_from_horizontal):
     # Convert to radians
     rad_horizontal = y_from_horizontal / 6 / 57.3
--- 2020Vision/pipeline.py
+++ 2020Vision/pipeline.py
@@ -5,8 +5,8 @@ import cv2
 def process(frame, config):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     hsvFiltered = cv2.inRange(hsv,
-                              (config.hsv.hue[0], config.hsv.sat[0], config.hsv.val[0]),
-                              (config.hsv.hue[1], config.hsv.sat[1], config.hsv.val[1]))
+                              (config.hsv.hue[0], config.hsv.saturation[0], config.hsv.value[0]),
+                              (config.hsv.hue[1], config.hsv.saturation[1], config.hsv.value[1]))
 
     contours, _ = cv2.findContours(hsvFiltered, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
