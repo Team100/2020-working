8ac88d3f563a6c674b2ce90be071e540f9e59cdd
JoseC <AidanS24@users.noreply.github.com>
2020-01-11 14:32:05 -0800

    colorMatchWithAMotor

--- JoseChavez/Color Match/src/main/java/frc/robot/Robot.java
+++ JoseChavez/Color Match/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
@@ -25,17 +27,18 @@ public class Robot extends TimedRobot {
    * Change the I2C port below to match the connection of your color sensor
    */
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private Victor m_motor = new Victor(2);
+  private boolean runMotor = false;
 
   /**
-   * A Rev Color Sensor V3 object is constructed with an I2C port as a 
-   * parameter. The device will be automatically initialized with default 
-   * parameters.
+   * A Rev Color Sensor V3 object is constructed with an I2C port as a parameter.
+   * The device will be automatically initialized with default parameters.
    */
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   /**
-   * A Rev Color Match object is used to register and detect known colors. This can 
-   * be calibrated ahead of time or during operation.
+   * A Rev Color Match object is used to register and detect known colors. This
+   * can be calibrated ahead of time or during operation.
    * 
    * This object uses a simple euclidian distance to estimate the closest match
    * with given confidence range.
@@ -43,8 +46,8 @@ public class Robot extends TimedRobot {
   private final ColorMatch m_colorMatcher = new ColorMatch();
 
   /**
-   * Note: Any example colors should be calibrated as the user needs, these
-   * are here as a basic example.
+   * Note: Any example colors should be calibrated as the user needs, these are
+   * here as a basic example.
    */
   private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
@@ -56,20 +59,20 @@ public class Robot extends TimedRobot {
     m_colorMatcher.addColorMatch(kBlueTarget);
     m_colorMatcher.addColorMatch(kGreenTarget);
     m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget);    
+    m_colorMatcher.addColorMatch(kYellowTarget);
   }
 
   @Override
   public void robotPeriodic() {
     /**
-     * The method GetColor() returns a normalized color value from the sensor and can be
-     * useful if outputting the color to an RGB LED or similar. To
-     * read the raw color, use GetRawColor().
+     * The method GetColor() returns a normalized color value from the sensor and
+     * can be useful if outputting the color to an RGB LED or similar. To read the
+     * raw color, use GetRawColor().
      * 
-     * The color sensor works best when within a few inches from an object in
-     * well lit conditions (the built in LED is a big help here!). The farther
-     * an object is the more light from the surroundings will bleed into the 
-     * measurements and make it difficult to accurately determine its color.
+     * The color sensor works best when within a few inches from an object in well
+     * lit conditions (the built in LED is a big help here!). The farther an object
+     * is the more light from the surroundings will bleed into the measurements and
+     * make it difficult to accurately determine its color.
      */
     Color detectedColor = m_colorSensor.getColor();
 
@@ -92,13 +95,67 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
-     * sensor.
+     * Open Smart Dashboard or Shuffleboard to see the color detected by the sensor.
      */
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
     SmartDashboard.putNumber("Confidence", match.confidence);
     SmartDashboard.putString("Detected Color", colorString);
+
+    String gameData;
+    gameData = DriverStation.getInstance().getGameSpecificMessage();
+    runMotor=true;
+    if (gameData.length() > 0) {
+      switch (gameData.charAt(0)) {
+      case 'B':
+        if(match.color == kBlueTarget){
+          runMotor=false;
+        }
+        // Blue case code
+        break;
+      case 'G':
+      if(match.color == kGreenTarget){
+        runMotor=false;
+      }
+        // Green case code
+        break;
+      case 'R':
+        if(match.color == kRedTarget){
+          runMotor=false;
+        }
+        // Red case code
+        break;
+      case 'Y':
+      if(match.color == kYellowTarget){
+        runMotor=false;
+      }
+        // Yellow case code
+        break;
+      default:
+        // This is corrupt data
+        break;
+      }
+    } else {
+      // Code for no data received yet
+    }
+    SmartDashboard.putString("Game Data", gameData);
   }
+
+  @Override
+  public void teleopPeriodic() {
+  
+    if(runMotor){
+      m_motor.set(1);
+    }
+    else{
+      m_motor.set(0);
+    }
+
+
+    
+
+  
+  }
+
 }
