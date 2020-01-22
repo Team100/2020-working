baa0a28a4ab60c7f7f8dfa79e94b60cc3e714289
Alex Beaver <799710@seq.org>
2020-01-21 17:58:22 -0800

    Update ExampleSubsystem.java

--- Alex B/ShooterTester/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ Alex B/ShooterTester/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,18 +7,51 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
+
+
+  public TalonSRX shooterMaster;
+
+  public TalonSRX shooterFollower;
+
   /**
    * Creates a new ExampleSubsystem.
    */
   public ExampleSubsystem() {
+    shooterMaster = new TalonSRX(0);
+    shooterFollower = new TalonSRX(1);
+    shooterFollower.follow(shooterMaster);
+    if(SmartDashboard.getNumber("percentOuput", -32767) != -32767){
+      publishSettingsToSmartDashboard();
+    }
+  }
 
+  public void publishSettingsToSmartDashboard(){
+    SmartDashboard.putNumber("percentOutput", 0);
+    SmartDashboard.putBoolean("Flush", false);
+
+
+  }
+
+  public void syncVelocityWithSmartDashboard(){
+    SmartDashboard.putNumber("Sensor Velocity", shooterMaster.getSelectedSensorVelocity());
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    if(SmartDashboard.getBoolean("Flush", false)){
+      double speed = SmartDashboard.getNumber("percentOutput", 0);
+      shooterMaster.set(ControlMode.PercentOutput, speed);
+      SmartDashboard.putBoolean("Flush", false);
+    }
+
   }
 }
