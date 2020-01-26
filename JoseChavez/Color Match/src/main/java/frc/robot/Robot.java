88591883408e5249504b017f949851ed1fb1493e
JoseC <AidanS24@users.noreply.github.com>
2020-01-26 14:50:46 -0800

    countingColors

--- JoseChavez/Color Match/src/main/java/frc/robot/Robot.java
+++ JoseChavez/Color Match/src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Victor;
+//import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorMatch;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 /**
  * This is a simple example to show how the REV Color Sensor V3 can be used to
@@ -28,8 +28,17 @@ public class Robot extends TimedRobot {
    * Change the I2C port below to match the connection of your color sensor
    */
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private WPI_TalonSRX m_motor = new WPI_TalonSRX(1);
+ // private final WPI_TalonSRX m_motor = new WPI_TalonSRX(1);
   private boolean runMotor = true;
+  private int redCounter = 0;
+  private int yellowCounter = 0;
+  private int blueCounter = 0;
+  private int greenCounter = 0;
+  private boolean redController = true;
+  private boolean blueController = true;
+  private boolean yellowController = true;
+  private boolean greenController = true;
+
 
   /**
    * A Rev Color Sensor V3 object is constructed with an I2C port as a parameter.
@@ -45,15 +54,14 @@ public class Robot extends TimedRobot {
    * with given confidence range.
    */
   private final ColorMatch m_colorMatcher = new ColorMatch();
-
-  /**
+ /**
    * Note: Any example colors should be calibrated as the user needs, these are
    * here as a basic example.
    */
-  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kBlueTarget = ColorMatch.makeColor(0.196, 0.491, 0.311);
   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private final Color kRedTarget = ColorMatch.makeColor(0.483, 0.371, 0.148);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.301, 0.550, 0.148);
 
   @Override
   public void robotInit() {
@@ -75,13 +83,15 @@ public class Robot extends TimedRobot {
      * is the more light from the surroundings will bleed into the measurements and
      * make it difficult to accurately determine its color.
      */
-    Color detectedColor = m_colorSensor.getColor();
+    final Color detectedColor = m_colorSensor.getColor();
 
     /**
      * Run the color match algorithm on our detected color
      */
     String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    
+    final ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+   
 
     if (match.color == kBlueTarget) {
       colorString = "Blue";
@@ -105,8 +115,56 @@ public class Robot extends TimedRobot {
     SmartDashboard.putString("Detected Color", colorString);
 
     String gameData;
+  
+   
     gameData = DriverStation.getInstance().getGameSpecificMessage();
+   
     runMotor=true;
+    
+    if(match.color == kRedTarget){
+      if (redController){
+        redCounter=0;
+        redController=false;
+      }
+      redCounter=redCounter+1;
+    }
+     else if(match.color!=kRedTarget){
+      redController=true;
+    }
+    if(match.color == kBlueTarget){
+      if (blueController){
+        blueCounter=0;
+        blueController=false;
+      }
+      blueCounter =blueCounter+1;
+    }
+     else if(match.color!=kBlueTarget){
+      blueController=true;
+    }
+    if(match.color == kGreenTarget){
+      if (greenController){
+        greenCounter=0;
+        greenController=false;
+      }
+      greenCounter =greenCounter+1;
+    }
+     else if(match.color!=kGreenTarget){
+      greenController=true;
+    }
+    if(match.color == kYellowTarget){
+      if(yellowController){
+        yellowCounter=0;
+        yellowController=false;
+      }
+      yellowCounter =yellowCounter+1;
+    }
+     else if(match.color!=kYellowTarget){
+      yellowController=true;
+    }
+     SmartDashboard.putNumber("Red Counter", redCounter);
+     SmartDashboard.putNumber("Blue Counter", blueCounter);
+     SmartDashboard.putNumber("Green Counter", greenCounter);
+     SmartDashboard.putNumber("Yellow Counter", yellowCounter);
     if (gameData.length() > 0) {
       switch (gameData.charAt(0)) {
       case 'B':
@@ -145,18 +203,14 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-  
+
     if(runMotor){
-      m_motor.set(1);
+    //  m_motor.set(1);
     }
     else{
-      m_motor.set(0);
+    //  m_motor.set(0);
     }
-
-
     
-
-  
   }
 
 }
