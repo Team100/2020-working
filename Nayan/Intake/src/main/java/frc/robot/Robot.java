e89042c9c8a562516ac7efe2e49bda734c5cdca7
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-01-29 16:31:42 -0800

    added sensor data

--- Nayan/Intake/src/main/java/frc/robot/Robot.java
+++ Nayan/Intake/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -62,7 +63,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void disabledInit() {
-    SmartDashboard.putString("First", "ran");
   }
 
   @Override
--- Nayan/Intake/src/main/java/frc/robot/subsystems/Indexer.java
+++ Nayan/Intake/src/main/java/frc/robot/subsystems/Indexer.java
@@ -2,6 +2,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -10,7 +11,8 @@ import frc.robot.Constants;
 public class Indexer extends SubsystemBase {
   private static WPI_VictorSPX leftSpx;
   private static WPI_VictorSPX rightSpx;
-  
+  DigitalInput frontSensor = new DigitalInput(1);
+  DigitalInput outSensor = new DigitalInput(0);
 
   public Indexer() {
     leftSpx = new WPI_VictorSPX(Constants.LEFT_SPX_CANID);
@@ -40,5 +42,7 @@ public static void RightFoward() {
     leftSpx.set(ControlMode.PercentOutput, 0.00);
     rightSpx.set(ControlMode.PercentOutput, 0.00);
 
+    SmartDashboard.putBoolean("frontSensor", frontSensor.get());
+    SmartDashboard.putBoolean("outSensor", outSensor.get());
   }
 }
