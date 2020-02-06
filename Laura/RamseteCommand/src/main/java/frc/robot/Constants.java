089ec47abb56604740a68091682791f9854b65a0
LauraRhodes <laura@pyxiseng.com>
2020-02-06 15:42:29 -0800

    delete unused constants

--- Laura/RamseteCommand/src/main/java/frc/robot/Constants.java
+++ Laura/RamseteCommand/src/main/java/frc/robot/Constants.java
@@ -20,12 +20,7 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 public final class Constants {
   public static final class DriveConstants {
     public static final int kLeftMotor1Port = 0;
-    public static final int kLeftMotor2Port = 1;
     public static final int kRightMotor1Port = 2;
-    public static final int kRightMotor2Port = 3;
-
-    public static final int[] kLeftEncoderPorts = new int[]{0, 1};
-    public static final int[] kRightEncoderPorts = new int[]{2, 3};
     public static final boolean kLeftEncoderReversed = false;
     public static final boolean kRightEncoderReversed = true;
 
