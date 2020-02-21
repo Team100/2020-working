c1f1dca957de55148d9adcee2caed768463d1204
LauraRhodes <laura@pyxiseng.com>
2020-02-20 17:55:07 -0800

    Made colors into an enum

--- Laura/RamseteCommand/src/main/java/frc/robot/subsystems/ColorSubsystem.java
+++ Laura/RamseteCommand/src/main/java/frc/robot/subsystems/ColorSubsystem.java
@@ -23,11 +23,43 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utility.T100ColorSensor;
 
 public class ColorSubsystem extends SubsystemBase {
+
+  public enum CP_Color {
+    RED(0.561, 0.232, 0.114, "Red"), 
+    YELLOW(0.361, 0.524, 0.113, "Yellow"), 
+    BLUE(0.143, 0.427, 0.429, "Blue"),
+    GREEN(0.197, 0.561, 0.240, "Green"),
+    UNKNOWN(0,0,0,"Unknown");
+
+    private double red_component;
+    private double green_component;
+    private double blue_component;
+    private Color color;
+    private String name;
+
+    CP_Color(double red_component, double green_component, double blue_component, String name) {
+      this.name = name;
+      update_components (red_component, green_component, blue_component);
+     }
+
+    void update_components(double R, double G, double B) {
+      this.red_component = R;
+      this.green_component = G;
+      this.blue_component = B;
+      this.color = ColorMatch.makeColor(this.red_component, this.green_component, this.blue_component);
+    }
+
+    Color getColor() {
+      return color;
+    }
+
+    String getName() {
+      return this.name;
+    }
+
+  }
+
   private final T100ColorSensor m_colorSensor = new T100ColorSensor();
-  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
   private String colorString = "Unknown";
   private boolean isRed = false;
   private boolean isYellow = false;
@@ -38,34 +70,25 @@ public class ColorSubsystem extends SubsystemBase {
    */
 
   public ColorSubsystem() {
-    m_colorSensor.addColorMatch(kBlueTarget);
-    m_colorSensor.addColorMatch(kGreenTarget);
-    m_colorSensor.addColorMatch(kRedTarget);
-    m_colorSensor.addColorMatch(kYellowTarget);
+    m_colorSensor.addColorMatch(CP_Color.BLUE.getColor());
+    m_colorSensor.addColorMatch(CP_Color.GREEN.getColor());
+    m_colorSensor.addColorMatch(CP_Color.RED.getColor());
+    m_colorSensor.addColorMatch(CP_Color.YELLOW.getColor());
     addChild("Color Sensor", m_colorSensor);
     ShuffleboardTab colorfulTab = Shuffleboard.getTab("Colorful");
     ShuffleboardLayout colorfulLayout = colorfulTab.getLayout("Sensor", BuiltInLayouts.kList);
     colorfulLayout.add("Color Sensor", m_colorSensor);
-    ShuffleboardLayout colorBooleansLayout = colorfulTab.getLayout("Current Color", BuiltInLayouts.kGrid)
-      .withSize(4,1)
-      .withPosition(0,0)
-      .withProperties(Map.of("Number of columns", 4, "Number of rows", 1, "Label position", "HIDDEN"));
-    colorBooleansLayout.addBoolean("IsRed", () -> isRed).withWidget(BuiltInWidgets.kBooleanBox)
-        .withPosition(0,0)
-        .withSize(1,1)
-        .withProperties(Map.of("Color when true", "Red", "Color when false", "White"));
-    colorBooleansLayout.addBoolean("IsYellow", () -> isYellow).withWidget(BuiltInWidgets.kBooleanBox)
-        .withPosition(1,0)
-        .withSize(1,1)
-        .withProperties(Map.of("Color when true", "Yellow", "Color when false", "White"));
-    colorBooleansLayout.addBoolean("IsGreen", () -> isGreen).withWidget(BuiltInWidgets.kBooleanBox)
-        .withPosition(2,0)
-        .withSize(1,1)
-        .withProperties(Map.of("Color when true", "Green", "Color when false", "White"));
-    colorBooleansLayout.addBoolean("IsBlue", () -> isBlue).withWidget(BuiltInWidgets.kBooleanBox)
-        .withPosition(3,0)
-        .withSize(1,1)
-        .withProperties(Map.of("Color when true", "Blue", "Color when false", "White"));
+    ShuffleboardLayout colorBooleansLayout = colorfulTab.getLayout("Current Color", BuiltInLayouts.kGrid).withSize(4, 1)
+        .withPosition(0, 0)
+        .withProperties(Map.of("Number of columns", 4, "Number of rows", 1, "Label position", "HIDDEN"));
+    colorBooleansLayout.addBoolean("IsRed", () -> isRed).withWidget(BuiltInWidgets.kBooleanBox).withPosition(0, 0)
+        .withSize(1, 1).withProperties(Map.of("Color when true", "Red", "Color when false", "White"));
+    colorBooleansLayout.addBoolean("IsYellow", () -> isYellow).withWidget(BuiltInWidgets.kBooleanBox).withPosition(1, 0)
+        .withSize(1, 1).withProperties(Map.of("Color when true", "Yellow", "Color when false", "White"));
+    colorBooleansLayout.addBoolean("IsGreen", () -> isGreen).withWidget(BuiltInWidgets.kBooleanBox).withPosition(2, 0)
+        .withSize(1, 1).withProperties(Map.of("Color when true", "Green", "Color when false", "White"));
+    colorBooleansLayout.addBoolean("IsBlue", () -> isBlue).withWidget(BuiltInWidgets.kBooleanBox).withPosition(3, 0)
+        .withSize(1, 1).withProperties(Map.of("Color when true", "Blue", "Color when false", "White"));
   }
 
   @Override
@@ -76,24 +99,25 @@ public class ColorSubsystem extends SubsystemBase {
     isBlue = false;
     isYellow = false;
     isGreen = false;
-    if (match.color == kBlueTarget) {
-      colorString = "Blue";
+    CP_Color current;
+    if (match.color == CP_Color.BLUE.getColor()) {
+      current = CP_Color.BLUE;
       isBlue = true;
-    } else if (match.color == kRedTarget) {
-      colorString = "Red";
+    } else if (match.color == CP_Color.RED.getColor()) {
+      current = CP_Color.RED;
       isRed = true;
-    } else if (match.color == kGreenTarget) {
-      colorString = "Green";
+    } else if (match.color == CP_Color.GREEN.getColor()) {
+      current = CP_Color.GREEN;
       isGreen = true;
-    } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
+    } else if (match.color == CP_Color.YELLOW.getColor()) {
+      current = CP_Color.YELLOW;
       isYellow = true;
     } else {
-      colorString = "Unknown";
+      current = CP_Color.UNKNOWN;
     }
 
     SmartDashboard.putNumber("Confidence", match.confidence);
-    SmartDashboard.putString("Detected Color", colorString);
+    SmartDashboard.putString("Detected Color", current.getName());
     SmartDashboard.putBoolean("Color Sensor Has Reset", m_colorSensor.hasReset());
     SmartDashboard.putBoolean("IsRed", isRed);
     SmartDashboard.putBoolean("IsYellow", isYellow);
