0848ac8b2492c2af011223144d8d1a3c8aca7469
Aidan Sacco <saccoa54@gmail.com>
2020-03-01 22:53:49 -0800

    Create Teleop Programs for each subsystem

---
+++ Aidan S/Bringup/All/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,83 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public final class OI {
+        public static final int LEFT_JOYSTICK_PORT = 0;
+        public static final int RIGHT_JOYSTICK_PORT = 1;
+        
+        public static final int TURRET_FORWARD = 6;
+        public static final int TURRET_REVERSE = 5;
+        public static final int TURRET_ZERO = 6;
+
+        public static final int SHOOTER_INCREASE = 8;
+        public static final int SHOOTER_DECREASE = 10;
+        public static final int SHOOTER_STOP = 12;
+
+        public static final int PIVOT_UP = 7;
+        public static final int PIVOT_DOWN = 9;
+        public static final int PIVOT_ZERO = 11;
+        public static final int SPIN = 1;
+        
+        public static final int STAGE_ONE_FORWARD = 6;
+        public static final int STAGE_ONE_REVERSE = 5;
+        public static final int STAGE_TWO_FORWARD = 6;
+        public static final int STAGE_TWO_REVERSE = 5;
+
+        public static final int SPINNER_FORWARD = 6;
+        public static final int SPINNER_REVERSE = 5;
+    }
+
+    public final class Turret {
+        public static final int MOTOR_CAN_ID = 8;
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    public final class Shooter {
+        public static final int MASTER_CAN_ID = 2;
+        public static final int FOLLOWER_CAN_ID = 13;
+    }
+
+    public final class Intake {
+        public static final int PIVOT_MOTOR_CAN_ID = 10;
+        public static final int SPIN_MOTOR_CAN_ID = 9;
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    public final class StageOne {
+        public static final int MOTOR_CAN_ID = 8;
+    }
+
+    public final class StageTwo {
+        public static final int MOTOR_CAN_ID = 7;
+    }
+
+    public final class Drivetrain {
+        public static final int LEFT_MASTER_CAN_ID = 0;
+        public static final int LEFT_FOLLOWER_CAN_ID = 1;
+        public static final int RIGHT_MASTER_CAN_ID = 15;
+        public static final int RIGHT_FOLLOWER_CAN_ID = 14;
+    }
+
+    public final class Spinner {
+        public static final int MOTOR_CAN_ID = 11;
+    }
+}
---
+++ Aidan S/Bringup/All/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,99 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.CPForward;
+import frc.robot.commands.CPReverse;
+import frc.robot.commands.Drive;
+import frc.robot.commands.Spin;
+import frc.robot.subsystems.*;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+    private Joystick leftStick = new Joystick(Constants.OI.LEFT_JOYSTICK_PORT);
+    private Joystick rightStick = new Joystick(Constants.OI.RIGHT_JOYSTICK_PORT);
+
+    private JoystickButton forward = new JoystickButton(leftStick, Constants.OI.TURRET_FORWARD);
+    private JoystickButton reverse = new JoystickButton(leftStick, Constants.OI.TURRET_REVERSE);
+    private JoystickButton zero = new JoystickButton(rightStick, Constants.OI.TURRET_ZERO);
+
+    private JoystickButton increase = new JoystickButton(leftStick, Constants.OI.SHOOTER_INCREASE);
+    private JoystickButton decrease = new JoystickButton(leftStick, Constants.OI.SHOOTER_DECREASE);
+    private JoystickButton stop = new JoystickButton(leftStick, Constants.OI.SHOOTER_STOP);
+
+    private JoystickButton pivotUp = new JoystickButton(rightStick, Constants.OI.PIVOT_UP);
+    private JoystickButton pivotDown = new JoystickButton(rightStick, Constants.OI.PIVOT_DOWN);
+    private JoystickButton pivotZero = new JoystickButton(rightStick, Constants.OI.PIVOT_ZERO);
+    private JoystickButton spin = new JoystickButton(rightStick, Constants.OI.SPIN);
+    
+    private JoystickButton cpforward = new JoystickButton(rightStick, Constants.OI.SPINNER_FORWARD);
+    private JoystickButton cpreverse = new JoystickButton(rightStick, Constants.OI.SPINNER_REVERSE);
+
+    // The robot's subsystems and commands are defined here...
+    private final Turret turret = new Turret();
+    private final Shooter shooter = new Shooter();
+    private final Intake intake = new Intake();
+    private final Drivetrain drivetrain = new Drivetrain();
+    private final Spinner spinner = new Spinner();
+
+    /**
+     * The container for the robot.  Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        drivetrain.setDefaultCommand(new Drive(drivetrain, leftStick, rightStick));
+
+        // Configure the button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Use this method to define your button->command mappings.  Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+     * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        forward.whenHeld(new InstantCommand(turret::forward));
+        reverse.whenHeld(new InstantCommand(turret::reverse));
+        zero.whenHeld(new InstantCommand(turret::zero));
+        
+        increase.whenHeld(new InstantCommand(shooter::increase));
+        decrease.whenHeld(new InstantCommand(shooter::decrease));
+        stop.whenHeld(new InstantCommand(shooter::stop));
+
+        pivotUp.whenHeld(new InstantCommand(intake::up));
+        pivotDown.whenHeld(new InstantCommand(intake::down));
+        pivotZero.whenHeld(new InstantCommand(intake::zero));
+        spin.whenHeld(new Spin(intake));
+
+        cpforward.whenHeld(new CPForward(spinner));
+        cpreverse.whenHeld(new CPReverse(spinner));
+    }
+
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return null;
+    }
+}
